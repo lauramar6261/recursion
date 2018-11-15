6a76aaf7964705e76e0d77f3a85920320e7fa446
Ada <larmartinez2@gmail.com>
2018-11-15 12:00:14 -0800

    recursion for binary search and linear search

--- recursion.rb
+++ recursion.rb
@@ -1,7 +1,12 @@
 
 def recursive_linear_search(find_me, index, list)
-# Your Code here
-
+  if index >= list.length
+    return -1
+  elsif find_me == list[index]
+    return index
+  else
+    return recursive_linear_search(find_me, index + 1, list)
+  end
 end
 
 def linear_search(find_me, list)
@@ -9,7 +14,15 @@ def linear_search(find_me, list)
 end
 
 def recursive_binary_search(find_me, start, last, list)
-
+  return -1 if start > last
+  mid = (start + last) / 2
+  if find_me == list[mid]
+    return mid
+  elsif find_me < list[mid]
+    return recursive_binary_search(find_me, start, mid - 1, list)
+  elsif find_me > list[mid]
+    return recursive_binary_search(find_me, mid + 1, last, list)
+  end
 end
 
 def binary_search(find_me, list)
